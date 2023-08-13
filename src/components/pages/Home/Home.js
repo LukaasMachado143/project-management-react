8611380e16350240702a53cae0074ef1db657cff
Lucas Fernando <lucas.fernando@predify.me>
2023-08-13 12:00:41 -0300

    Home estilizada.

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,12 @@
-function Home(){
-    return <h1>Home</h1>
+import styles from './Home.module.css'
+import savings from '../../../img/savings.svg'
+function Home() {
+    return (
+        <section className={styles.homeContainer}>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Comece  gerenciar seus projetps agora mesmo</p>
+            <img src={savings}></img>
+        </section>
+    )
 }
 export default Home
