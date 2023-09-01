0fadd71a9c7e896afbae7aaa960e57aa93385904
Lucas Machado <lukaasmachado143@gmail.com>
2023-08-31 22:11:47 -0300

    preparado página para receber os projetos.

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,8 @@
 import styles from "./Projects.module.css"
 import Message from "../../components/Message/Message"
 import { useLocation } from "react-router-dom"
+import LinkButton from "../../components/LinkButton/LinkButton"
+import Container from "../../components/Container/Container"
 function Projects() {
     const location = useLocation()
     let message = ''
@@ -10,9 +12,15 @@ function Projects() {
         type = location.state.type
     }
     return (
-        <div>
-            <h1>Projects</h1>
-            {message && type && <Message timeOut={3500} type={type} msg={message} />}
+        <div className={styles.projectContainer}>
+            <div className={styles.title}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto' />
+            </div>
+            <Container customClass="start">
+                {message && type && <Message timeOut={3500} type={type} msg={message} />}
+                <p>Projetos...</p>
+            </Container>
 
         </div>
     )
